b9da3c3c34649187442cae637b826f914dbecb73
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2024-06-05 21:28:07 +0700

    add controller functions

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Ingredient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
 
 class IngredientController extends Controller
 {
@@ -13,7 +16,8 @@ class IngredientController extends Controller
      */
     public function index(Request $request)
     {
-        //
+        $ingredient = Ingredient::all();
+        return view('admin.ingredient.ingredient', compact('ingredient'));
     }
 
     public function totalIngredient()
@@ -27,7 +31,7 @@ class IngredientController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.ingredient.ingredient-add');
     }
 
     /**
@@ -35,38 +39,62 @@ class IngredientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        $validatedData = $validator->validated();
+        Ingredient::create($validatedData);
+        $this->totalIngredient();
+
+        return redirect()->route('ingredient.index')->with('status', 'Data added successfully');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Ingredient $ingredient)
+    public function show($id)
     {
-        //
+        $ingredient = Ingredient::findOrFail($id);
+        return view('admin.ingredient.ingredient-detail', ['ingredient' => $ingredient]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Ingredient $ingredient)
+    public function edit($id)
     {
-        //
+        $ingredient = Ingredient::findOrFail($id);
+        return view('admin.ingredient.ingredient-edit', compact('ingredient'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Ingredient $ingredient)
+    public function update(Request $request, $id)
     {
-        //
+        $ingredient = Ingredient::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        $ingredient->update([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('ingredient.index')->with('success', 'Data updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Ingredient $ingredient)
+    public function destroy(string $id)
     {
-        //
+        $ingredient = Ingredient::findOrFail($id);
+        $ingredient->destroy();
+        $this->totalIngredient();
+
+        return redirect()->route('ingredient.index');
     }
 }
