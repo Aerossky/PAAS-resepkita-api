1e15c3abe98ac5b08e8fbe13abb1ef392f2df84f
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2024-06-05 19:38:36 +0700

    add role_id to user table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,12 +20,14 @@ class UserSeeder extends Seeder
                 'name' => 'John Doe',
                 'email' => 'john@gmail.com',
                 'password' => Hash::make('anjay123'),
+                'role_id' => 2, // 2 for 'user', 1 for 'admin
                 'api_key' => '123456789',
             ],
             [
                 'name' => 'admin',
                 'email' => 'admin@gmail.com',
                 'password' => Hash::make('anjay123'),
+                'role_id' => 1,
                 'api_key' => '123456798',
             ],
         ];
@@ -36,6 +38,7 @@ class UserSeeder extends Seeder
                 'name' => $item['name'],
                 'email' => $item['email'],
                 'password' => $item['password'],
+                'role_id' => $item['role_id'],
                 'api_key' => $item['api_key'],
             ]);
         }
