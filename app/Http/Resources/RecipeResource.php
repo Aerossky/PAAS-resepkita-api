bdda472d47b0a4c9714d781bec2add0863db5dc0
ilhamrisqi <92148866+ilhamrisqi@users.noreply.github.com>
2024-06-05 15:13:26 +0700

    Respone Template
    membuat template respon recipe

--- app/Http/Resources/RecipeResource.php
+++ app/Http/Resources/RecipeResource.php
@@ -14,6 +14,13 @@ class RecipeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+         return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'instructions' => $this->instructions,
+            'ingredients' => $this->ingredients->pluck('name'),
+            'created_at' => $this->created_at,
+        ];
     }
 }
