176bacfba6f9a45c20a116b16765d2b72f26a382
Stefanus Reynaldo <stefanusr4@gmail.com>
2024-06-06 17:32:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ export default {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                quicksand: ["Quicksand"],
+            },
             colors: {
+                "resep-kuning": {
+                    100: "#FAEBCD",
+                    200: "#E1EB71",
+                    300: "#F7C873",
+                },
+                "resep-merah": {
+                    100: "#ECAB69",
+                    200: "#E36161",
+                },
+                "resep-hitam": "#434343",
                 customRed: "#E36161",
                 customOrange: "#ECAB69",
                 customOrangeTua: "#F28B21",
